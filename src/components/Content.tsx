c24e14fe812f7c4f259dc10013dbd8a508c44d32
rickutino <rickutino@gmail.com>
2021-03-17 23:10:10 +0900

    refactoring with context api

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
+import { MovieCard } from '../components/MovieCard';
+import  { useMovie } from '../context/MovieContext';
+
 export function Content() {
-  // Complete aqui
+  const { selectedGenre, movies } = useMovie();
+  return(
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard 
+              key={movie.Title}
+              title={movie.Title} 
+              poster={movie.Poster} 
+              runtime={movie.Runtime} 
+              rating={movie.Ratings[0].Value} 
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,26 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+import  { useMovie } from '../context/MovieContext';
+
+export default function SideBar() {
+  const { genres, handleClickButton, selectedGenreId } = useMovie();
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
